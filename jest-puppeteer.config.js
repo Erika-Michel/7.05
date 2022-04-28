24463e2ca40c761912d3d52dda86408a38d95934
Erika-Michel <erikamichel174@gmail.com>
2022-04-28 17:58:33 +0200

    Initial commit

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    launch: {
+        headless: false,
+        defaultViewport: null,
+//        slowMo: 1000,
+        args: ['--start-maximized'] //— используем максимальный размер окна браузера
+      },
+    
+  };
